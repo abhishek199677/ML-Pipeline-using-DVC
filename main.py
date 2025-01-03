14df4bcc3152c03d419b9efa3a6327c24e4370f5
abhishek199677 <108387488+abhishek199677@users.noreply.github.com>
2025-01-03 18:31:02 +0530

    main.py not necessary

---
+++ main.py
@@ -0,0 +1,19 @@
+# import os
+
+# os.system("python src/data_ingestion.py")
+# print("=============> Data_ingestion created <=============")
+
+# os.system("python src/data_preprocessing.py")
+# print("=============> Data_preprocessing created <=============")
+
+# os.system("python src/feature_engineering.py")
+# print("=============> Feature engineering created <=============")
+
+
+# os.system("python src/model_building.py")
+# print("=============> model_building created <=============")
+
+
+
+# os.system("python src/model_evaluation.py")
+# print("=============> model_evaluation created <=============")
